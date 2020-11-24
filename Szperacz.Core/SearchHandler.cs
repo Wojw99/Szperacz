382b77be512699b75c01a3d516e6371c70801441
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-11-24 16:50:22 +0100

    MainViewModel update.

---
+++ Szperacz.Core/SearchHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Szperacz.Core
+{
+    public static class SearchHandler
+    {
+        public static List<String> SearchWord(string word, string path)
+        {
+            var list = new List<String>();
+
+            // In this place a function will be calling the python script, waiting for answer and returning a result
+            // For it returns example paths
+            list.Add("D:\\Users\\Pulpit\\tekst.txt");
+            list.Add("D:\\Users\\Pulpit\\guf.pdf");
+            list.Add("D:\\Users\\Pulpit\\tekst2.doc");
+
+            return list;
+        }
+    }
+}
--- Szperacz.Core/ViewModels/MainViewModel.cs
+++ Szperacz.Core/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using MvvmCross.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
 using Szperacz.Core.Models;
 
@@ -11,6 +13,65 @@ namespace Szperacz.Core.ViewModels
 {
     public class MainViewModel : MvxViewModel
     {
+        private string _path;
+        private string _word;
+        private ObservableCollection<String> _outputPathList = new ObservableCollection<String>();
 
+        public MainViewModel()
+        {
+            SearchCommand = new MvxCommand(SearchWord);
+        }
+
+        // Connected to the search button
+        public IMvxCommand SearchCommand { get; set; }
+        public void SearchWord()
+        {
+            if(IsCorrectPath(Path) && Word.Length > 0)
+            {
+                var pathsWithWord = SearchHandler.SearchWord(Word, Path);
+                OutputPathList = new ObservableCollection<String>(pathsWithWord);
+            }
+            else
+            {
+                // TODO: send a warning messege to the user
+            }
+        }
+
+        private bool IsCorrectPath(string path)
+        {
+            var drives = DriveInfo.GetDrives();
+            var driveNames = new List<string>();
+
+            foreach (var d in drives) driveNames.Add(d.Name);
+
+            if (path.Length < 4) return false;
+            if (!driveNames.Contains(path.Substring(0, 3))) return false;
+
+            return true;
+        }
+
+        public ObservableCollection<String> OutputPathList
+        {
+            get { return _outputPathList; }
+            set { SetProperty(ref _outputPathList, value); }
+        }
+
+        public string Path
+        {
+            get { return _path; }
+            set
+            {
+                SetProperty(ref _path, value);
+            }
+        }
+
+        public string Word
+        {
+            get { return _word; }
+            set
+            {
+                SetProperty(ref _word, value);
+            }
+        }
     }
 }
