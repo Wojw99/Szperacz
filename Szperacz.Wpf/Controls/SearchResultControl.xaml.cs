69a77f2f831fc74661504c33bdc1fe3b0d2f47f1
Wojw99 <42806302+Wojw99@users.noreply.github.com>
2020-12-10 09:28:36 +0100

    Organized.

---
+++ Szperacz.Wpf/Controls/SearchResultControl.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Szperacz.Wpf.Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy SearchResultControl.xaml
+    /// </summary>
+    public partial class SearchResultControl : UserControl
+    {
+        public SearchResultControl()
+        {
+            InitializeComponent();
+        }
+
+        public object PathResult
+        {
+            get { return (object)GetValue(PathResultProperty); }
+            set { SetValue(PathResultProperty, value); }
+        }
+
+        
+        public static readonly DependencyProperty PathResultProperty =
+            DependencyProperty.Register("PathResult", typeof(object), typeof(SearchResultControl), new PropertyMetadata(0));
+
+        public object ButtonOpenClick
+        {
+            get { return (object)GetValue(ButtonOpenClickProperty); }
+            set { SetValue(ButtonOpenClickProperty, value); }
+        }
+
+        public static readonly DependencyProperty ButtonOpenClickProperty =
+            DependencyProperty.Register("ButtonOpenClick", typeof(object), typeof(SearchResultControl), new PropertyMetadata(0));
+    }
+}
