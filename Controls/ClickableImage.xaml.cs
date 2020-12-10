9c4ee7cbc6b32970cad51d56d44d18c106fed740
AdamW6556 <61738820+AdamW6556@users.noreply.github.com>
2020-12-10 17:48:37 +0100

    Modified listbox control

---
+++ Controls/ClickableImage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Szperacz.Wpf.Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ClickableImage.xaml
+    /// </summary>
+    public partial class ClickableImage : UserControl
+    {
+
+        public object ImagePath
+        {
+            get { return (object)GetValue(ImagePathProperty); }
+            set 
+            {
+                back.Background = Brushes.Gray;
+                SetValue(ImagePathProperty, value);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for Source.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImagePathProperty =
+            DependencyProperty.Register("ImagePath", typeof(object), typeof(ClickableImage), new PropertyMetadata(0));
+
+        public ClickableImage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Controls/ExplorerBox.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Szperacz.Wpf.Controls
+{
+    /// <summary>
+    /// Logika interakcji dla klasy ExplorerBox.xaml
+    /// </summary>
+    public partial class ExplorerBox : UserControl
+    {
+        #region Properies
+        public object BorderColor
+        {
+            get { return (object)GetValue(BorderColorProperty); }
+            set { SetValue(BorderColorProperty, value); }
+        }
+
+        public object TextWatermark
+        {
+            get { return (object)GetValue(TextWatermarkProperty); }
+            set { SetValue(TextWatermarkProperty, value); }
+        }
+
+        public object TextInside
+        {
+            get { return (object)GetValue(TextInsideProperty); }
+            set { SetValue(TextInsideProperty, value); }
+        }
+
+        public object ExItemSource
+        {
+            get { return (object)GetValue(ExItemSourceProperty); }
+            set { SetValue(ExItemSourceProperty, value); }
+        }
+        #endregion
+
+        #region Dependencies
+        public static readonly DependencyProperty BorderColorProperty =
+            DependencyProperty.Register("BorderColor", typeof(object), typeof(ExplorerBox), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty TextWatermarkProperty =
+            DependencyProperty.Register("TextWatermark", typeof(object), typeof(ExplorerBox), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty TextInsideProperty =
+            DependencyProperty.Register("TextInside", typeof(object), typeof(ExplorerBox), new PropertyMetadata(0));
+
+        public static readonly DependencyProperty ExItemSourceProperty =
+            DependencyProperty.Register("ExItemSource", typeof(object), typeof(ExplorerBox), new PropertyMetadata(0));
+        #endregion
+
+        public ExplorerBox()
+        {
+            InitializeComponent();
+            BorderColor = Brushes.Gray;
+        }
+
+        public void Alert()
+        {
+            BorderColor = Brushes.Red;
+        }
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(combo.SelectedIndex > -1)
+            {
+                TextInside = combo.SelectedItem.ToString();
+            }
+        }
+
+        private void textBoxPath_GotFocus(object sender, RoutedEventArgs e)
+        {
+            textBlockWatermark.Visibility = Visibility.Hidden;
+        }
+
+        private void textBoxPath_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(textBoxPath.Text == "")
+                textBlockWatermark.Visibility = Visibility.Visible;
+        }
+
+        private void textBoxPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBoxPath.Text == "")
+            {
+                textBlockWatermark.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                textBlockWatermark.Visibility = Visibility.Hidden;
+            }
+
+            BorderColor = Brushes.Gray;
+            combo.SelectedIndex = -1;
+        }
+    }
+}
