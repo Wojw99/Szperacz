b15e7e482fd1d2a9a6ba3441efef2af117e8a997
Greenexe <47354749+Greenexe@users.noreply.github.com>
2020-12-18 16:32:15 +0100

    Create nkn.py

---
+++ nkn.py
@@ -0,0 +1,14 @@
+#0.5
+
+dni_wolne=[[1, 1],[6, 1],[4, 4],[5, 4],[1, 5],[3, 5],[23, 5],[3, 6],[15, 8],[1, 10],[11, 10],[25, 12],[26, 12]]
+
+        #ilosc miesiecy, etat, start poczatek miesiaca
+    
+    
+roboty=[6,0.5,1]
+okulary=[18,'n',5]
+ekonomiczne=[18,'n',5]
+
+
+
+
